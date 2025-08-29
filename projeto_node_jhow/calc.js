d674b65620cef02e3f85dd609b26fee9633b01f1
jhow. <jhonathan.pereira@fatec.sp.gov.br>
2025-08-29 20:55:01 -0300

    Delete projeto_node_jhow directory

--- projeto_node_jhow/calc.js
+++
@@ -1,5 +0,0 @@
-var funcSoma = require ("./somar");
-var funcSub = require ("./sub");
-
-console.log(funcSoma(10,5));
-console.log(funcSub(60,20));
--- projeto_node_jhow/sub.js
+++
@@ -1,4 +0,0 @@
-var sub = function (v1,v2){
- return v1-v2;
-}
-module.exports = sub;
